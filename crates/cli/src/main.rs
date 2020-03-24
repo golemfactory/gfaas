b49785b3954096c67451d14600df1ca4b65e33cb
Jakub Konka <jakub.konka@golem.network>
2020-03-24 16:06:08 +0100

    Cleanup build and add run and clean commands

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::path::Path;
 use std::process::{Command, Stdio};
 use structopt::{clap::AppSettings, StructOpt};
 
@@ -37,51 +38,101 @@ enum Subcommand {
         #[structopt()]
         args: Vec<String>,
     },
-    Clean,
+    Clean {
+        /// Pass additional arguments directly to cargo run command
+        #[structopt()]
+        args: Vec<String>,
+    },
 }
 
 fn main() {
     let opt = Opt::from_args();
     // Get cwd
     let cwd = env::current_dir().unwrap();
-
     match opt.cmd {
-        Subcommand::Build { release, args } => {
-            // Specify output dir
-            let out_dir = cwd.join(format!(
-                "target/{}",
-                if release { "release" } else { "debug" }
-            ));
-            // Run cargo build
-            let mut cmd = Command::new("cargo");
-            cmd.arg("build")
-                // TODO We don't want the user to pass `--release` using aux cargo args,
-                // so let's filter it out for now. In the future, we might want to
-                // throw an error instead.
-                .args(
-                    args.into_iter()
-                        .filter(|x| x != "--release" && !x.contains("--target-dir")),
-                )
-                .env("CARGO_TARGET_DIR", "target")
-                .env("GFAAS_OUT_DIR", &out_dir)
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit());
-            if release {
-                cmd.arg("--release");
-            }
-            let _cmd_out = cmd.output().unwrap();
-            // Next, run cargo rustc --target=wasm32-unknown-emscripten on marked
-            // files.
-            let mut cmd = Command::new("rustc");
-            cmd.arg("+1.38.0")
-                .arg("--target=wasm32-unknown-emscripten")
-                .arg("gfaas.rs")
-                .stdout(Stdio::inherit())
-                .stderr(Stdio::inherit())
-                .current_dir(out_dir);
-            let _cmd_out = cmd.output().unwrap();
-        }
-        Subcommand::Run { .. } => {}
-        Subcommand::Clean { .. } => {}
+        Subcommand::Build { release, args } => build(&cwd, release, &args),
+        Subcommand::Run { release, args } => run(&cwd, release, &args),
+        Subcommand::Clean { args } => clean(&cwd, &args),
     }
+}
+
+fn build(cwd: &Path, release: bool, args: &Vec<String>) {
+    // Specify output dir
+    let out_dir = cwd.join(format!(
+        "target/{}",
+        if release { "release" } else { "debug" }
+    ));
+    // Run cargo build
+    let mut cmd = Command::new("cargo");
+    cmd.arg("build")
+        // TODO We don't want the user to pass `--release` using aux cargo args,
+        // so let's filter it out for now. In the future, we might want to
+        // throw an error instead.
+        .args(
+            args.iter()
+                .filter(|x| x.as_str() != "--release" && !x.contains("--target-dir")),
+        )
+        .env("CARGO_TARGET_DIR", "target")
+        .env("GFAAS_OUT_DIR", &out_dir)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+    if release {
+        cmd.arg("--release");
+    }
+    let _cmd_out = cmd.output().unwrap();
+    // Next, run cargo rustc --target=wasm32-unknown-emscripten on marked
+    // files.
+    let mut cmd = Command::new("rustc");
+    cmd.arg("+1.38.0")
+        .arg("--target=wasm32-unknown-emscripten")
+        .arg("gfaas.rs")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .current_dir(out_dir);
+    if release {
+        cmd.arg("-O");
+    } else {
+        cmd.arg("-g");
+    }
+    let _cmd_out = cmd.output().unwrap();
+}
+
+fn run(cwd: &Path, release: bool, args: &Vec<String>) {
+    // We need to run cargo build first so that the Wasm artifacts are properly
+    // generated.
+    build(cwd, release, args);
+    // Specify output dir
+    let out_dir = cwd.join(format!(
+        "target/{}",
+        if release { "release" } else { "debug" }
+    ));
+    // Run cargo run
+    let mut cmd = Command::new("cargo");
+    cmd.arg("run")
+        // TODO We don't want the user to pass `--release` using aux cargo args,
+        // so let's filter it out for now. In the future, we might want to
+        // throw an error instead.
+        .args(
+            args.iter()
+                .filter(|x| x.as_str() != "--release" && !x.contains("--target-dir")),
+        )
+        .env("CARGO_TARGET_DIR", "target")
+        .env("GFAAS_OUT_DIR", &out_dir)
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit());
+    if release {
+        cmd.arg("--release");
+    }
+    let _cmd_out = cmd.output().unwrap();
+}
+
+fn clean(cwd: &Path, args: &Vec<String>) {
+    let mut cmd = Command::new("cargo");
+    cmd.arg("clean")
+        .args(args.iter().filter(|x| !x.contains("--target-dir")))
+        .env("CARGO_TARGET_DIR", "target")
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .current_dir(cwd);
+    let _cmd_out = cmd.output().unwrap();
 }
