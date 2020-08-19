2cf2a3df8082e9a58ed7aa72d243204f16577c11
Jakub Konka <kubkon@golem.network>
2020-08-19 16:01:35 +0200

    Fix sum example to handle fallible stream

--- examples/sum/src/main.rs
+++ examples/sum/src/main.rs
@@ -1,5 +1,6 @@
+use anyhow::Result;
 use gfaas::remote_fn;
-use futures::stream::{self, StreamExt};
+use futures::stream::{self, TryStreamExt};
 
 #[remote_fn(
     datadir = "/Users/kubkon/dev/yagna/ya-req",
@@ -10,16 +11,18 @@ fn partial_sum(r#in: Vec<u64>) -> u64 {
 }
 
 #[actix_rt::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     let input: Vec<u64> = (0..100).collect();
-    let input: Vec<_> = input.chunks(50).collect();
+    let input: Vec<Result<_>> = input.chunks(50).map(|x| Ok(x)).collect();
     let input = stream::iter(input);
 
-    let output = input.fold(0u64, |acc, x| async move {
-        let out = partial_sum(x.to_vec()).await;
-        acc + out
+    let output = input.try_fold(0u64, |acc, x| async move {
+        let out = partial_sum(x.to_vec()).await?;
+        Ok(acc + out)
     });
     let output = output.await?;
     println!("{:?}", output);
     assert_eq!((0..100).sum::<u64>(), output);
+
+    Ok(())
 }
